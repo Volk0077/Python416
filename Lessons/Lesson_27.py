0546c9fdd2eafd155e001caccdc73b7de2cc23ee
Volk0077 <volk0077@mail.ru>
2025-04-15 22:02:05 +0300

    наследование

--- Lessons/Lesson_27.py
+++ Lessons/Lesson_27.py
@@ -54,35 +54,35 @@ class Figure:
 
 
 class Rectangle(Figure):
-    def __init__(self, width, height, color):
-        self.__width = width
-        self.__height = height
+        def __init__(self, width, height, color):
         super().__init__(color)
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, w):
-        if isinstance(w, int) and  w > 0:
+        if isinstance(w, int) and w > 0:
             self.__width = w
         else:
-            raise ValueError('Ширина должна быть положительным числом')
-        
+            raise ValueError("Ширина должна быть положительным числом")
+
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, h):
         if isinstance(h, int) and h > 0:
-            self._height = h
+            self.__height = h
         else:
-            raise ValueError('Высота должна быть положительным числом')
+            raise ValueError("Высота должна быть положительным числом")
 
     def area(self):
-        print(f"Площадь {self.color} прямоугольника:", end=" " )
+        print(f"Площадь {self.color} прямоугольника:", end=" ")
         return self.__width * self.__height
 
 
@@ -91,34 +91,61 @@ print(rect.area())
 '''
 
 
+
+
 # Задача
 
 
-class Rect:    
-    def __init__(self, width, height):       
-        self.width = width        
-        self.height = height           
+# class Rect:    
+#     def __init__(self, width, height):       
+#         self.width = width        
+#         self.height = height           
 
-    def show_rect(self):       
-        print(f"Прямоугольник:\nШирина: {self.width}\nВысота: {self.height}")
-
-
-class RectFon(Rect):
-    def __init__(self, width, height, background):
-        super().__init__(width, height)
-        self.fon = background
-
-    def show_rect(self):
-        super().show_rect()
-        print("Фон:", self.fon)
-
-# Домашка-----------------------------------------------------------------------------------------------
-class RectBorder(Rect):
-    ...
+#     def show_rect(self):       
+#         print(f"Прямоугольник:\nШирина: {self.width}\nВысота: {self.height}")
 
 
+# class RectFon(Rect):
+#     def __init__(self, width, height, background):
+#         super().__init__(width, height)
+#         self.fon = background
 
-shape1 = RectFon(400, 200, "yellow")
-shape1.show_rect()
+#     def show_rect(self):
+#         super().show_rect()
+#         print("Фон:", self.fon)
+
 # # Домашка-----------------------------------------------------------------------------------------------
-shape2 = RectBorder(600, 300, "1px", 'solid', 'red')
+# class RectBorder(Rect):
+#     ...
+
+
+
+# shape1 = RectFon(400, 200, "yellow")
+# shape1.show_rect()
+# # # Домашка-----------------------------------------------------------------------------------------------
+# shape2 = RectBorder(600, 300, "1px", 'solid', 'red')
+
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def set_coord(self, x=None, y=None):
+        if y is None:
+            self.x = x
+        elif x is None:
+            self.y = y
+        else:
+            self.x = x
+            self.y = y
+
+
+p1 = Point(5, 10)
+print(p1.__dict__)
+p1.set_coord(20, 30)
+print(p1.__dict__)
+p1.set_coord(50)
+print(p1.__dict__) p1.set_coord(y=100)
+print(p1.__dict__)
