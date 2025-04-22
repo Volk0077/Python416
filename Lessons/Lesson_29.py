6b4d5bde042ebac89a63acaa24673ce7d63d3ed6
Volk0077 <volk0077@mail.ru>
2025-04-22 21:36:11 +0300

    Перегрузка оператора

--- Lessons/Lesson_29.py
+++ Lessons/Lesson_29.py
@@ -86,8 +86,6 @@ l1.draw()
 
 # _____________Миксины___________
 '''
-
-'''
 class MixinLog:
     ID = 0
 
@@ -119,4 +117,60 @@ class Notebook(Goods, MixinLog):
 n = Notebook("HP", 1.5, 35000)
 n.print_info()
 n.save_sell_log()
+'''
+
+#  ___________________Перегрузка оператора______________________
+
+
+# Число секунд в одном дне: 24 * 60 * 60 = 86400
+
+class Clock:
+    __DAY = 86400
+
+    def __init__(self, sec: int):
+        if not isinstance(sec, int):
+            raise ValueError("Секунды должны быть целым числом")
+        self.sec = sec % self.__DAY
+
+    def get_format_time(self):
+        s = self.sec % 60
+        m = (self.sec // 60) % 60
+        h = (self.sec // 3600) % 24
+        return f"{Clock.__get_form(h)}:{Clock.__get_form(m)}:{Clock.__get_form(s)}"
+
+    @staticmethod
+    def __get_form(x):
+        return str(x) if x > 9 else "0" + str(x)
+
+    def __add__(self, other):
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом Clock")
+        return Clock(self.sec + other.sec)
+
+    def __eq__(self, other):
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом Clock")
+        return self.sec == other.sec
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+c1 = Clock(100)
+c2 = Clock(200)
+c3 = c1 + c2
+print(c1.get_format_time())
+print(c2.get_format_time())
+# c1 += c2
+print(c3.get_format_time())
+print(c1.get_format_time())
+if c1 == c2:
+    print("Время равно")
+else:
+    print("Время разное")
+
+if c1 != c2:
+    print("Время разное")
+else:
+    print("Время равно")
 
