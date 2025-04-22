acef4d1dd01219acfca891d3df790e4b14454cb2
Volk0077 <volk0077@mail.ru>
2025-04-22 21:36:50 +0300

    class Student

--- Lessons/Lesson_29.py
+++ Lessons/Lesson_29.py
@@ -124,53 +124,65 @@ n.save_sell_log()
 
 # Число секунд в одном дне: 24 * 60 * 60 = 86400
 
-class Clock:
-    __DAY = 86400
+# class Clock:
+#     __DAY = 86400
 
-    def __init__(self, sec: int):
-        if not isinstance(sec, int):
-            raise ValueError("Секунды должны быть целым числом")
-        self.sec = sec % self.__DAY
+#     def __init__(self, sec: int):
+#         if not isinstance(sec, int):
+#             raise ValueError("Секунды должны быть целым числом")
+#         self.sec = sec % self.__DAY
 
-    def get_format_time(self):
-        s = self.sec % 60
-        m = (self.sec // 60) % 60
-        h = (self.sec // 3600) % 24
-        return f"{Clock.__get_form(h)}:{Clock.__get_form(m)}:{Clock.__get_form(s)}"
+#     def get_format_time(self):
+#         s = self.sec % 60
+#         m = (self.sec // 60) % 60
+#         h = (self.sec // 3600) % 24
+#         return f"{Clock.__get_form(h)}:{Clock.__get_form(m)}:{Clock.__get_form(s)}"
 
-    @staticmethod
-    def __get_form(x):
-        return str(x) if x > 9 else "0" + str(x)
+#     @staticmethod
+#     def __get_form(x):
+#         return str(x) if x > 9 else "0" + str(x)
 
-    def __add__(self, other):
-        if not isinstance(other, Clock):
-            raise ArithmeticError("Правый операнд должен быть типом Clock")
-        return Clock(self.sec + other.sec)
+#     def __add__(self, other):
+#         if not isinstance(other, Clock):
+#             raise ArithmeticError("Правый операнд должен быть типом Clock")
+#         return Clock(self.sec + other.sec)
 
-    def __eq__(self, other):
-        if not isinstance(other, Clock):
-            raise ArithmeticError("Правый операнд должен быть типом Clock")
-        return self.sec == other.sec
+#     def __eq__(self, other):
+#         if not isinstance(other, Clock):
+#             raise ArithmeticError("Правый операнд должен быть типом Clock")
+#         return self.sec == other.sec
 
-    def __ne__(self, other):
-        return not self.__eq__(other)
+#     def __ne__(self, other):
+#         return not self.__eq__(other)
 
 
-c1 = Clock(100)
-c2 = Clock(200)
-c3 = c1 + c2
-print(c1.get_format_time())
-print(c2.get_format_time())
-# c1 += c2
-print(c3.get_format_time())
-print(c1.get_format_time())
-if c1 == c2:
-    print("Время равно")
-else:
-    print("Время разное")
+# c1 = Clock(100)
+# c2 = Clock(200)
+# c3 = c1 + c2
+# print(c1.get_format_time())
+# print(c2.get_format_time())
+# # c1 += c2
+# print(c3.get_format_time())
+# print(c1.get_format_time())
+# if c1 == c2:
+#     print("Время равно")
+# else:
+#     print("Время разное")
 
-if c1 != c2:
-    print("Время разное")
-else:
-    print("Время равно")
+# if c1 != c2:
+#     print("Время разное")
+# else:
+#     print("Время равно")
 
+class Student:
+    def __init__(self, name, *marks):
+        self.name = name
+        self.marks = list(marks)
+
+    def __getitem__(self, item):
+        return self.marks[item]
+
+
+s1 = Student("Сергей", 5, 5, 3, 4, 5)
+# print(s1.marks[2])
+print(s1[2])
