792543b153f0dfb6e37876dfabac1b7363adf2c0
Volk0077 <volk0077@mail.ru>
2025-05-22 19:45:17 +0300

    L 35 Парсинг с использованием классов

---
+++ Lessons/Lesson_35.py
@@ -0,0 +1,19 @@
+# Парсинг с использованием классов
+#  создаем новый файл parser.py, в этой же папке!
+
+'''
+from parser import Parser # импортируем созданный Класс
+
+
+def main():
+    pars = Parser("https://www.ixbt.com/live/index/news/", "news.txt")
+    pars.run()
+
+
+if __name__ == "__main__":
+    main()
+'''
+
+#  Модуль MVC - модель, вид (представление), контроллер
+
+
