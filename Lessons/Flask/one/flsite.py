e8cb2bf457b1d67c3f38d43f2208d24d79fb0674
Volk0077 <volk0077@mail.ru>
2025-06-19 21:51:29 +0300

    L_41: Flask DB

--- Lessons/Flask/one/flsite.py
+++ Lessons/Flask/one/flsite.py
@@ -1,8 +1,9 @@
 # Flask
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'qweqweqweqweweqwq'
 
 menu = [
     {"name": "Главная", "url": "index"},
@@ -25,8 +26,12 @@ def about():
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
     if request.method == "POST":
-        print(request.form)
-        print(request.form['username'])
+        # print(request.form)
+        # print(request.form['username'])
+        if len(request.form['username']) > 2:
+            flash("Сообщение отправлено успешно!", category="success")
+        else:
+            flash("Ошибка отправки", category="error")
     return render_template("contact.html", title="Обратная связь", menu=menu)
 
 
@@ -35,6 +40,9 @@ def profile(username):
     return f"Пользователь: {username}"
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page404.html', title="Страница не найдена", menu=menu), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ Lessons/Flask/two/sitedb.py
@@ -0,0 +1,60 @@
+import sqlite3, os
+from flask import Flask, render_template, flash, request, g
+# from fdatabase import FDataBase
+
+
+#  Конфигурация
+DATABASE = '/tmp/flsk.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsk.db')))
+
+
+# Подключение к БД
+def connect_db():
+    con = sqlite3.connect(app.config['DATABASE'])
+    con.row_factory = sqlite3.Row
+    return con
+
+
+# Создание БД
+def create_db(): # участвует в коде только при первом запуске
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.route('/')
+def index():
+    db =  get_db()
+    dbase = FDataBase(db)
+    return render_template('index.html', menu=dbase.get_menu())
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+@app.route("/add_post", methods=["POST", "GET"])
+def add_post():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    return render_template('add_post.html', menu=dbase.get_menu())
+    
+
+if __name__ == '__main__':
+    app.run()
