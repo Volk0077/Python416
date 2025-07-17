701ecc08237811cc241fe1ebf475be03aa56c8c2
Volk0077 <volk0077@mail.ru>
2025-07-17 21:42:45 +0300

    L_49: Django ->  Обновлены настройки админки, шаблоны и маршруты проектов; добавлен шаблон single-project.html и директория media для хранения файлов(через admin). Реализованы новые функции отображения и управления проектами.

--- fourth/projects/views.py
+++ fourth/projects/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import Project
+
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    pr = Project.objects.all()
+    context = {'projects': pr}
+    return render(request, 'projects/projects.html', context)
+
+
+def project(request, pk):
+    project_obj = Project.objects.get(id=pk)
+    return render(request, 'projects/single-project.html', {'project': project_obj})
