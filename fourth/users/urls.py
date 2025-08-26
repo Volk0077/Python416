924a347ef43d3a4f91fda8adc16286d1cbac138f
Volk0077 <volk0077@mail.ru>
2025-08-26 15:34:15 +0300

    L_53:Django -> реализована старница регистарции пользователей, создана страница  account

--- fourth/users/urls.py
+++ fourth/users/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.profiles, name='profiles'),
     path('profile/<str:pk>/', views.user_profile, name='user_profile'),
+    path('account/', views.user_account, name='account'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('edit_account/', views.edit_account, name='edit_account'),
+    path('register/', views.register_user, name='register'),
 ]
